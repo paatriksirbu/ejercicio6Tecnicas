105b4ea10671e381d818818cbaf4a139da2a9be9
Patrik Paul Sirbu <psirb@myuax.com>
2024-02-20 18:24:55 +0100

    Ejercicio 1 completo

--- src/ejercicio1/FuncionesNumeros.java
+++ src/ejercicio1/FuncionesNumeros.java
@@ -2,7 +2,7 @@ package ejercicio1;
 
 public class FuncionesNumeros {
 
-    public String clasificar(int a, int b) {
+    public String clasificar(int a, int b) {    //Metodo que recibe dos numeros y los compara para saber si son iguales, si el primero es mayor o si el segundo es mayor y devuelve el valor correspondiente.
         if (a == b ) {
             return "0";
         } else if (a > b) {
@@ -12,7 +12,7 @@ public class FuncionesNumeros {
         }
     }
 
-    public int mayor(int a, int b) {
+    public int mayor(int a, int b) {    //Metodo que recibe dos numeros y los compara para saber cual es el mayor
         if (a > b) {
             return a;
         } else {
--- src/ejercicio1/SistemaComparacionClasificacion.java
+++ src/ejercicio1/SistemaComparacionClasificacion.java
@@ -3,12 +3,19 @@ import java.util.*;
 
 
 public class SistemaComparacionClasificacion {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //Metodo principal que crea un objeto de la clase FuncionesNumeros y llama a los metodos mayor y clasificar
         FuncionesNumeros clasificar = new FuncionesNumeros();
+        Scanner sc = new Scanner(System.in);
 
+        //Pedimos los numeros al usuario y despues procedemos a llamar a los metodos de la clase FuncionesNumeros.
+        System.out.println("Ingrese el primer número: ");
+        int a = sc.nextInt();
 
+        System.out.println("Ingrese el segundo número: ");
+        int b = sc.nextInt();
 
+        System.out.println("El número mayor es: " + clasificar.mayor(a, b) + " y la clasificacion es: " + clasificar.clasificar(a, b));
 
-
+        sc.close();
     }
 }
