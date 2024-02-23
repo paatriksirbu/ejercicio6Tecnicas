01c170269a034bc36c449d16d00b3c2c5881cb59
Patrik <psirb@myuax.com>
2024-02-23 20:35:10 +0100

    Modificaciones SOLID. La clase tablero ahora solo tendra lo que es la representacion de este mismo y algunos estados como agua o tocado y hundido.

--- src/ejercicio2/Tablero.java
+++ src/ejercicio2/Tablero.java
@@ -40,37 +40,6 @@ public class Tablero {
         System.out.println("+");
     }
 
-    public static int[] elegirUbicaciónAleatoria(char[][] tablero) {    // Elegir ubicación aleatoria
-        Random rand = new Random();
-        int fila = rand.nextInt(tablero.length);
-        int columna = rand.nextInt(tablero[0].length);
-        return new int[]{fila, columna};
-    }
-
-    public static int[] obtenerIntento() {  // Obtener intento del usuario
-        Scanner scanner = new Scanner(System.in);
-        int fila = 0;
-        int columna = 0;
-        boolean entradaValida = false;
-
-        while (!entradaValida){
-            try{
-                System.out.print("Ingrese fila (1-4): ");
-                fila = scanner.nextInt();
-                System.out.print("Ingrese columna (1-4): ");
-                columna = scanner.nextInt();
-                if (fila >= 1 && fila <= 4 && columna >= 1 && columna <= 4){
-                    entradaValida = true;
-                } else {
-                    System.out.println("Entrada inválida. Ingrese un número entre 1 y 4.");
-                }
-            } catch (Exception e){
-                System.out.println("Entrada inválida. Ingrese SOLO numeros.");
-                scanner.nextLine();
-            }
-        }
-        return new int[]{fila, columna};
-    }
 
     public static void marcarAgua(char[][] tablero, int[] intento) {
         tablero[intento[0] - 1][intento[1] - 1] = 'A'; // A de Agua
@@ -81,38 +50,6 @@ public class Tablero {
     }
 
 
-    public static boolean disparar(char[][] tablero, int[] intento, int[] ubicación_barco) {
-        if (intento[0] - 1 == ubicación_barco[0] && intento[1] - 1 == ubicación_barco[1]) {
 
-            marcarTocadoYHundido(tablero, intento);
-            mostrarTablero(tablero);
-            return true;
-        } else {
-            marcarAgua(tablero, intento);
-            mostrarTablero(tablero);
-            return false;
-        }
-    }
-    public static void jugarBatallaNaval() {
-        // Jugar Batalla Naval
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Bienvenido a Batalla Naval!");
-        System.out.println("Introduzca el nombre del jugador: ");
-        String nombre = sc.nextLine();
-
-
-        char[][] tablero = crearTablero(4, 4);
-        int[] ubicación_barco = elegirUbicaciónAleatoria(tablero);
-        int[] intento;
-        boolean juegoTerminado = false;
-
-        do {
-            intento = obtenerIntento();
-            juegoTerminado = disparar(tablero, intento, ubicación_barco);
-            if (juegoTerminado) {
-                System.out.println("¡Felicidades " + nombre + "!" + "¡Hundiste el barco!");
-            }
-        } while (!juegoTerminado);
-    }
 
 }
